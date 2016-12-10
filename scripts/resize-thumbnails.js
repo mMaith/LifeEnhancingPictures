ac5d75077ce37dc1511408575fc5324a5d8b6c8b
mMaith <ber.wie@gmx.de>
2016-12-10 01:36:00 +0100

    Create resize-thumbnails.js

---
+++ scripts/resize-thumbnails.js
@@ -0,0 +1,11 @@
+// Größe der Thumbnails ändern
+function resizeThumb(e, b) {
+			var c = document.getElementById(e),
+				d = c.getElementsByTagName("img");
+			for (var a = 0; a < d.length; a++) {
+				d[a].src = d[a].src.replace(/\/s72\-c/, "/s" + b + "");
+				d[a].width = b;
+				d[a].height = b
+			}
+		}
+resizeThumb("PopularPosts1", 320);
