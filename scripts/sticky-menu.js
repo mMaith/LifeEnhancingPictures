673c8c6ecaf5f885c263c5c801f01369cfd4d0ba
mMaith <ber.wie@gmx.de>
2016-12-10 01:17:30 +0100

    Update sticky-menu.js

--- scripts/sticky-menu.js
+++ scripts/sticky-menu.js
@@ -1,3 +1,5 @@
+//Script für ein Sticky-Menü.
+// Im Moment nicht benötigt, so geil ist mein Menü jetzt nicht...
 $(document).ready(function(){
 	var stickyNavTop=$('.nav-menu').offset().top;
 	var stickyNav=function() {
