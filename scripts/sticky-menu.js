57b0b96165400bfca0668d274cbbb7f0878baa30
mMaith <ber.wie@gmx.de>
2016-12-10 01:17:13 +0100

    Update sticky-menu.js

--- scripts/sticky-menu.js
+++ scripts/sticky-menu.js
@@ -1,13 +1,15 @@
 $(document).ready(function(){
-			var stickyNavTop=$('.nav-menu').offset().top;
-			var stickyNav=function(){
-				var scrollTop=$(window).scrollTop();
-				if(scrollTop>stickyNavTop){
-					$('.nav-menu').addClass('sticky')
-				} else {$('.nav-menu').removeClass('sticky')}
-			};
-			stickyNav();
-			$(window).scroll(function(){
-				stickyNav()
-			})
-		});
+	var stickyNavTop=$('.nav-menu').offset().top;
+	var stickyNav=function() {
+		var scrollTop=$(window).scrollTop();
+		if(scrollTop>stickyNavTop){
+			$('.nav-menu').addClass('sticky')
+		} else {
+			$('.nav-menu').removeClass('sticky')
+		}
+	};
+	stickyNav();
+	$(window).scroll(function(){
+		stickyNav()
+	})
+});
